4a23c5f3cafda2138a70b00b6aff01d198831201
pranjalpatel044 <pranjalpatelpatel4@gmail.com>
2026-03-12 11:25:57 +0530

    deploy all

--- src/main/java/com/demo/helloworld/controller/HelloController.java
+++ src/main/java/com/demo/helloworld/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayhello(){
-           return "Hello World....I am learning CI/CD pipeline currently";
+           return "Hello World....I am learning CI/CD pipeline";
     }
 }
